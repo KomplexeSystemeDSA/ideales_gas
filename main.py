17cd83ae4048cb359b02598825afddbeb0b09e66
matteolutz <matteo.lutz06@outlook.de>
2022-08-16 09:28:03 +0200

    added new collision system

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 import pygame
 
@@ -12,7 +14,7 @@ class Atom:
         self.mass = mass
         self.radius = radius
 
-    def check_collision(self, other: Atom):
+    def check_collision(self, other):
         distance_vec = other.pos - self.pos
         distance_vec_mag = distance_vec.magnitude()
         min_distance = self.radius + other.radius
@@ -24,12 +26,50 @@ class Atom:
         correction_vector = d.normalize() * distance_correction
         other.pos += correction_vector
         self.pos -= correction_vector
-        """sum_mass = a.mass + a2.mass
-        a.vel -= ((2 * a2.mass) / sum_mass) * (((a.vel - a2.vel) * (a.pos - a2.pos)) / (a.pos - a2.pos).magnitude_squared()) * (a.pos - a2.pos)
-        a2.vel -= ((2 * a.mass) / sum_mass) * (
-                    ((a2.vel - a.vel) * (a2.pos - a.pos)) / (a2.pos - a.pos).magnitude_squared()) * (
-                             a2.pos - a.pos)"""
-        distance_correction = ()
+
+        theta = math.atan2(distance_vec.x, distance_vec.y)
+        sine = math.sin(theta)
+        cosine = math.cos(theta)
+
+        b_temp = [pygame.math.Vector2(), pygame.math.Vector2()]
+        b_temp[1].x = cosine * distance_vec.x + sine * distance_vec.y
+        b_temp[1].y = cosine * distance_vec.y - sine * distance_vec.x
+
+        v_temp = [pygame.math.Vector2(), pygame.math.Vector2()]
+
+        v_temp[0].x = cosine * self.vel.x + sine * self.vel.y
+        v_temp[0].y = cosine * self.vel.y - sine * self.vel.x
+        v_temp[1].x = cosine * other.vel.x + sine * other.vel.y
+        v_temp[1].y = cosine * other.vel.y - sine * other.vel.x
+
+        v_final = [pygame.math.Vector2(), pygame.math.Vector2()]
+
+        v_final[0].x = ((self.mass - other.mass) * v_temp[0].x + 2 * other.mass * v_temp[1].x) / (self.mass + other.mass)
+        v_final[0].y = v_temp[0].y
+
+        v_final[1].x = ((other.mass - self.mass) * v_temp[1].x + 2 * self.mass * v_temp[0].x) / (self.mass + other.mass)
+        v_final[1].y = v_temp[1].y
+
+        b_temp[0].x += v_final[0].x
+        b_temp[1].x += v_final[1].x
+
+        b_final = [pygame.math.Vector2(), pygame.math.Vector2()]
+        b_final[0].x = cosine * b_temp[0].x - sine * b_temp[0].x
+        b_final[0].y = cosine * b_temp[0].y + sine * b_temp[0].x
+        b_final[1].x = cosine * b_temp[1].x - sine * b_temp[1].y
+        b_final[1].y = cosine * b_temp[1].y + sine * b_temp[1].x
+
+        other.pos.x = self.pos.x + b_final[1].x
+        other.pos.y = self.pos.y + b_final[1].y
+
+        self.pos += b_final[0]
+
+        self.vel.x = cosine * v_final[0].x - sine * v_final[0].y
+        self.vel.y = cosine * v_final[0].y + sine * v_final[0].x
+        other.vel.x = cosine * v_final[1].x - sine * v_final[1].y
+        other.vel.y = cosine * v_final[1].y + sine * v_final[1].x
+
+        return True
 
     def update(self, dt: float):
         self.pos += self.vel * dt
@@ -39,7 +79,7 @@ class Atom:
 
 pygame.init()
 
-screen = pygame.display.set_mode((500, 500))
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 running = True
 clock = pygame.time.Clock()
@@ -51,7 +91,7 @@ clock = pygame.time.Clock()
 atoms = []
 ATOM_MASS = 1
 ATOM_RADIUS = 5
-for i in range(500):
+for i in range(1000):
     atoms.append(Atom(
         pygame.math.Vector2((np.random.randint(ATOM_RADIUS, WIDTH - ATOM_RADIUS, dtype=int), np.random.randint(ATOM_RADIUS, HEIGHT - ATOM_RADIUS, dtype=int))),
         pygame.math.Vector2(((np.random.rand() * 0.5) - 0.25, (np.random.rand() * 0.5) - 0.25)),
@@ -75,10 +115,12 @@ while running:
         if a.pos.y <= a.radius or a.pos.y >= HEIGHT - a.radius:
             a.vel.y *= -1
 
+        black_list = []
         for a2 in atoms:
-            if a == a2:
+            if a == a2 or a2 in black_list:
                 continue
-            a.check_collision(a2)
+            if a.check_collision(a2):
+                black_list.append(a2)
 
         a.update(dt)
         a.draw(screen)
